0aa400a7faf938ca752a2c9aacb3b81a6db048c1
Pawel Sobaniec <infipablosoban@gmail.com>
2023-03-04 16:28:04 +0100

    add my github

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="text-neutral-400  h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t-2 border-neutral-800 mt-5 flex sm:flex-row flex-col justify-center items-center px-3 space-y-3 sm:mb-0 mb-3">
-      <div className="text-center">
+    <footer className="text-neutral-400  h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t-2 border-neutral-800 mt-5 flex sm:flex-row flex-col justify-between items-center px-3 sm:mb-0 mb-3">
+      <div>
         Powered by{" "}
         <a
           href="https://openai.com/"
@@ -21,6 +21,16 @@ export default function Footer() {
           className="font-bold hover:underline transition underline-offset-2"
         >
           Vercel Edge Functions.
+        </a>
+      </div>
+      <div>
+      <a
+          href="https://github.com/infiplaya/"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold hover:underline transition underline-offset-2"
+        >
+          My Github
         </a>
       </div>
     </footer>
